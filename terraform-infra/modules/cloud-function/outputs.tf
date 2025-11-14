7171312a3617b25a825fa47a6eae63e393b76727
math974 <mathias.ballot@epitech.eu>
2025-11-14 17:31:38 +0100

    fix(api): delete path to the openapi

--- terraform-infra/modules/cloud-function/outputs.tf
+++ terraform-infra/modules/cloud-function/outputs.tf
@@ -1,6 +1,6 @@
 output "function_url" {
   description = "URL publique HTTP de la fonction"
-  value       = try(google_cloudfunctions2_function.function.url, google_cloudfunctions2_function.function.service_config[0].uri)
+  value       = google_cloudfunctions2_function.function.url
 }
 
 
