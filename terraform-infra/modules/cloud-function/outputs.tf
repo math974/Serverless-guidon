49ec856cfba0eed6295518e9a6d74ab840542efd
math974 <mathias.ballot@epitech.eu>
2025-11-27 16:54:41 +0100

    feat: add output for the module function

--- terraform-infra/modules/cloud-function/outputs.tf
+++ terraform-infra/modules/cloud-function/outputs.tf
@@ -3,5 +3,15 @@ output "function_url" {
   value       = google_cloudfunctions2_function.function.url
 }
 
+output "function_name" {
+  description = "Nom de la Cloud Function créée"
+  value       = google_cloudfunctions2_function.function.name
+}
+
+output "service_config" {
+  description = "Configuration du service Cloud Run"
+  value       = google_cloudfunctions2_function.function.service_config
+}
+
 
 
