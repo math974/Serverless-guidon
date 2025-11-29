d9bd3b559be742e6f45d01795e97bab32e85e232
math974 <mathias.ballot@epitech.eu>
2025-11-29 01:22:37 +0100

    feat: terraform

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -5,11 +5,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0"
+      version = ">= 5.0, < 8.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.0"
+      version = ">= 5.0, < 8.0"
     }
     archive = {
       source  = "hashicorp/archive"
--- terraform-infra/modules/app-engine/main.tf
+++ terraform-infra/modules/app-engine/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = ">= 5.0, < 8.0"
     }
     archive = {
       source  = "hashicorp/archive"
